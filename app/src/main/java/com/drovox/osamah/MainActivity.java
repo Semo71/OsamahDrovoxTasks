5e4f6dd5b625b65b2bb58963d70e846f40dfe81a
Osamah Fatah <o.fatah@ksb-it.com>
2022-10-16 16:48:37 +0300

    Initial Commit.

---
+++ app/src/main/java/com/drovox/osamah/MainActivity.java
@@ -0,0 +1,14 @@
+package com.drovox.osamah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
