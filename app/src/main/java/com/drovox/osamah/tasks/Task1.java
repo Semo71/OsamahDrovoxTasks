b615e9697d25aef36478a42107f8dd8cd998d861
osamahfatah <semo_717@hotmail.com>
2022-10-17 08:12:20 +0300

    Added Task1.

---
+++ app/src/main/java/com/drovox/osamah/tasks/Task1.java
@@ -0,0 +1,13 @@
+package com.drovox.osamah.tasks;
+
+public class Task1 {
+
+
+    /*
+    * Primitive Data Types: int, boolean, double..
+    * Non Primitive Data Types: Integer, Boolean, Double..
+    * HTTP verbs: GET, POST, Request, Response..
+    *
+    * The main difference between GET and POST: GET is to request data and POST is to send data.
+    * */
+}
