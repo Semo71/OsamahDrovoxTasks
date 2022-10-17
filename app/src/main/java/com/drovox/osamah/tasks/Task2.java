d7b3508397be3d24b1030a84a56f74006feefee3
Osamah Fatah <o.fatah@ksb-it.com>
2022-10-17 13:17:14 +0300

    Optimize Task2.

--- app/src/main/java/com/drovox/osamah/tasks/Task2.java
+++ app/src/main/java/com/drovox/osamah/tasks/Task2.java
@@ -1,13 +1,28 @@
 package com.drovox.osamah.tasks;
 
+import java.util.*;
+import java.util.Map.Entry;
+
 public class Task2 {
 
     public static int getFirstNonRepeatingIndex(String input) {
-        for (char i : input.toCharArray()) {
-            if (input.indexOf(i) == input.lastIndexOf(i)) {
-                return input.indexOf(i);
-            }
+        Map<Character, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < input.length(); i++) {
+
+            //In case user enters uppercase
+            char ch = Character.toLowerCase(input.charAt(i));
+
+            if (hashMap.containsKey(ch))
+                hashMap.put(ch, -1);
+            else
+                hashMap.put(ch, i);
         }
+
+        for (Entry<Character, Integer> entry : hashMap.entrySet()) {
+            if (entry.getValue() != -1)
+                return entry.getValue();
+        }
+
         return -1;
     }
 
